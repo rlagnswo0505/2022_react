3d7bee3531a33b9da4f321b62cc8db2b7006824b
rlagnswo0505 <rlagnswo0505@naver.com>
2022-07-02 23:46:44 +0900

    컴포넌트 사용하기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 // import logo from './logo.svg';
 import './App.css';
-
+function Header() {
+  return (
+    <header>
+      <h1>
+        <a href="/">WEB</a>
+      </h1>
+    </header>
+  );
+}
+function Nav() {
+  return (
+    <nav>
+      <ol>
+        <li>
+          <a href="/read/1">html</a>
+        </li>
+        <li>
+          <a href="/read/1">css</a>
+        </li>
+        <li>
+          <a href="/read/1">js</a>
+        </li>
+      </ol>
+    </nav>
+  );
+}
+function Article() {
+  return (
+    <article>
+      <h2>Welcom</h2>
+      Hello, WEB
+    </article>
+  );
+}
 function App() {
-  return <div className="App">Hello React</div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <Nav></Nav>
+      <Article></Article>
+    </div>
+  );
 }
 
 export default App;
