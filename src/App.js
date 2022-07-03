83ff55a53f90a3ff4b7fa59e516c60c49febd310
rlagnswo0505 <rlagnswo0505@naver.com>
2022-07-03 13:05:16 +0900

    컴포넌트에 클릭 이벤트 주기

--- src/App.js
+++ src/App.js
@@ -4,28 +4,20 @@ function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
 }
-function Nav() {
-  return (
-    <nav>
-      <ol>
-        <li>
-          <a href="/read/1">html</a>
-        </li>
-        <li>
-          <a href="/read/2">css</a>
-        </li>
-        <li>
-          <a href="/read/3">js</a>
-        </li>
-      </ol>
-    </nav>
-  );
-}
+
 function Article(props) {
   return (
     <article>
@@ -34,11 +26,49 @@ function Article(props) {
     </article>
   );
 }
+function Nav(props) {
+  const lis = [];
+  props.topics.map((li) =>
+    lis.push(
+      <li key={li.id}>
+        <a
+          href={'/read/' + li.id}
+          onClick={(e) => {
+            e.preventDefault();
+            props.onChangeMode(li.id);
+          }}
+        >
+          {li.title}
+        </a>
+      </li>
+    )
+  );
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
 function App() {
+  const topics = [
+    { id: 1, title: 'HTML', body: 'HTML is ...' },
+    { id: 2, title: 'CSS', body: 'CSS is ...' },
+    { id: 3, title: 'JavaScript', body: 'JavaScript is ...' },
+  ];
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav></Nav>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          alert('Header');
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
